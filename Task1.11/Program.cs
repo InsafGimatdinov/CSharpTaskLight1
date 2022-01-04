8abc782d4d78c97791d471e12f9ed7e9a21bd048
InsafGimatdinov <insaf.gimatdinov@mail.ru>
2022-01-04 15:59:49 +0300

    Add Files Task 1.1 - 1.13 Light Level

---
+++ Task1.11/Program.cs
@@ -0,0 +1,10 @@
+﻿// Составить программу вывода столбиком четртыех любых чисел.
+
+// Сгененрировали четыре случайных числа.
+int A = new Random().Next(1, 50);
+int B = new Random().Next(1, 40);
+int C = new Random().Next(1, 50);
+int D = new Random().Next(1, 40);
+
+// Вывод одинаковый с предыдущими числами.
+System.Console.WriteLine($"{A}\n{B}\n{C}\n{D}");
